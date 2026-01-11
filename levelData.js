e9cd04622a3bfc232875083634804cebcbd7bb62
David <dawudddkhannn@gmail.com>
2026-01-11 10:08:51 +0530

    Add files via upload

---
+++ levelData.js
@@ -0,0 +1,20 @@
+export const levels = [
+  {
+    id: 1,
+    name: "Level 1",
+    task: "Meditate for 10 minutes",
+    progress: 0
+  },
+  {
+    id: 2,
+    name: "Level 2",
+    task: "Focus on deep work session",
+    progress: 0
+  },
+  {
+    id: 3,
+    name: "Level 3",
+    task: "Journal your thoughts",
+    progress: 0
+  }
+];
